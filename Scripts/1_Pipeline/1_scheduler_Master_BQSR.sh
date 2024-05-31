7f69733eb0cd996ee2f24b9a687a22c95f50a105
KosterSCA <100853317+KosterSCA@users.noreply.github.com>
2024-05-31 20:09:09 +0200

    Create 1_scheduler_Master_BQSR.sh

---
+++ Scripts/1_Pipeline/1_scheduler_Master_BQSR.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --partition=cpu-long
+#SBATCH --output=output_%j.txt
+#SBATCH --time=1-00:00:00
+#SBATCH --mem=100GB
+#SBATCH --error=error_output_%j.txt
+#SBATCH --job-name=Lissotriton
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=stephaniekoster222@gmail.com
+
+cd /data1/s2321041/Lissotriton/
+
+module load skewer
+module load VCFtools
+module load BWA
+module load SAMtools
+module load picard
+
+perl /data1/s2321041/Lissotriton/Scripts/1_Master_BQSR.pl

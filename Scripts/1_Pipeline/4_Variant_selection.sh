ba185b619a9d8b024f5084163e44fe3a1a5d4cd1
KosterSCA <100853317+KosterSCA@users.noreply.github.com>
2024-07-15 15:35:43 +0200

    Create 4_Variant_selection.sh

---
+++ Scripts/1_Pipeline/4_Variant_selection.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --partition=cpu_natbio
+#SBATCH --output=output_%j.txt
+#SBATCH --time=0-01:00:00
+#SBATCH --error=error_output_%j.txt
+#SBATCH --job-name=variant_selection
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=stephaniekoster222@gmail.com
+
+cd /data1/s2321041/Lissotriton/variants/
+
+module load VCFtools
+
+vcftools --vcf Lissotriton_Exchet.vcf --max-missing 0.5 --remove-indels --minQ 20 --recode --recode-INFO-all --out Lisso0.5_filtered
+
+perl /data1/s2321041/Lissotriton/Scripts/4_subsampleVCF.pl Lisso0.5_filtered.recode.vcf Lisso0.5_SNPs_Subset.vcf

9f3a5b22f9b3bd57a0c0beb98bf800774923b9dd
KosterSCA <100853317+KosterSCA@users.noreply.github.com>
2024-07-15 15:38:16 +0200

    Create 5_Quality_filtering.sh

---
+++ Scripts/1_Pipeline/5_Quality_filtering.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --partition=cpu_natbio
+#SBATCH --output=output_%j.txt
+#SBATCH --time=0-01:00:00
+#SBATCH --error=error_output_%j.txt
+#SBATCH --job-name=quality_filtering
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=stephaniekoster222@gmail.com
+
+cd /data1/s2321041/Lissotriton/variants
+
+module load VCFtools
+
+vcftools --vcf Lissotriton_Exchet.vcf --max-missing 1 --remove-indels --minQ 20 --recode --recode-INFO-all --out Lisso1
+
+perl /data1/s2321041/Lissotriton/Scripts/5_SNP_Subset.pl Lisso1.recode.vcf Lisso1_SNPs_Subset.vcf

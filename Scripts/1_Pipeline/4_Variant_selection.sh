220fb1c91e154edd4181092fa4e36361c35c1178
KosterSCA <100853317+KosterSCA@users.noreply.github.com>
2024-07-15 15:38:37 +0200

    Update 4_Variant_selection.sh

--- Scripts/1_Pipeline/4_Variant_selection.sh
+++ Scripts/1_Pipeline/4_Variant_selection.sh
@@ -13,6 +13,6 @@ cd /data1/s2321041/Lissotriton/variants/
 
 module load VCFtools
 
-vcftools --vcf Lissotriton_Exchet.vcf --max-missing 0.5 --remove-indels --minQ 20 --recode --recode-INFO-all --out Lisso0.5_filtered
+vcftools --vcf Lissotriton_Exchet.vcf --max-missing 0.5 --remove-indels --minQ 20 --recode --recode-INFO-all --out Lisso0.5
 
-perl /data1/s2321041/Lissotriton/Scripts/4_subsampleVCF.pl Lisso0.5_filtered.recode.vcf Lisso0.5_SNPs_Subset.vcf
+perl /data1/s2321041/Lissotriton/Scripts/4_subsampleVCF.pl Lisso0.5.recode.vcf Lisso0.5_SNPs_Subset.vcf

02eb9b6d83fe4419fa733c4d9029dc1c9c989a91
KosterSCA <100853317+KosterSCA@users.noreply.github.com>
2024-07-15 17:27:34 +0200

    Create 2_treePL.sh

---
+++ Scripts/4_treePL/2_treePL.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --partition=cpu_natbio
+#SBATCH --output=output_%j.txt
+#SBATCH --time=0-01:00:00
+#SBATCH --error=error_output_%j.txt
+#SBATCH --job-name=TreePL_Lissotriton
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kostersca@vuw.leidenuniv.nl
+
+cd /data1/s2321041/Lissotriton/treePL/
+
+source /cm/shared/easybuild/software/AMUSE-Miniconda2/4.7.10/etc/profile.d/conda.sh
+conda activate /data1/s2321041/treepl
+
+/data1/s2321041/treepl/treePL 2_Lissotriton_treePL.txt

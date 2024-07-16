59440d0ba10718c772d859907ed9b52a76535b1b
KosterSCA <100853317+KosterSCA@users.noreply.github.com>
2024-07-16 15:44:36 +0200

    Create SNAPPER.sh

---
+++ Scripts/6_SNAPPER/SNAPPER.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+#SBATCH --ntasks=1
+#SBATCH --time=3-12:00:00
+#SBATCH --cpus-per-task=20
+#SBATCH --mem-per-cpu=10G
+#SBATCH --partition=cpu-long
+#SBATCH --output=output_%j.txt
+#SBATCH --error=error_output_%j.txt
+#SBATCH --job-name=SNAPPER_Lissotriton
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=kostersca@vuw.leidenuniv.nl
+
+cd /data1/s2321041/Lissotriton/Snapper/
+
+/data1/s2321041/beast/bin/beast -threads $SLURM_CPUS_PER_TASK LissotritonOutgroup.xml
